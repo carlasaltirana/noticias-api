97d5d939373726277c3e100b64b413f855b83877
carlasaltirana <saltirana.carla@gmail.com>
2022-12-04 16:34:01 +0100

    Added import and styles project

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -5,10 +5,7 @@ import CardMedia from '@mui/material/CardMedia'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid' 
-import  Box  from '@mui/material/box'
 import { border } from '@mui/system'
-
- 
 
 const Noticia = ({noticia}) => {
 
@@ -16,11 +13,8 @@ const Noticia = ({noticia}) => {
     return (
       
       <Grid item xs={6} md={3} ld={4} >
-        <Card style={{ height: '100%',background: '#f5f5dc '  }}>
+        <Card style={{ height: '100%',background: '#9D7166 ' }}>
           <CardMedia 
-            sx = {{
-               filter: 'gray'
-                 }}
             component ="img"
             alt ={`Imagen de la noticia ${title}`}
             image ={urlToImage}
